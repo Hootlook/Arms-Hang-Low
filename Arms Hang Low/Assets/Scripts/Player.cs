ad149650b3149d08c28167a242af8e02e64f0f2c
Hootlook <leetboom@live.fr>
2019-10-27 09:48:13 +0100

    Both grabbs fixed + refactoring

--- Arms Hang Low/Assets/Scripts/Player.cs
+++ Arms Hang Low/Assets/Scripts/Player.cs
@@ -6,16 +6,16 @@ public class Player : MonoBehaviour
 {
     public Transform rightHand;
     public Transform leftHand;
+    Collider2D[] rightColliders;
+    Collider2D[] leftColliders;
     Rigidbody2D righHandRB;
     Rigidbody2D leftHandRB;
     Rigidbody2D playerRB;
     bool isLeftGrabbing;
     bool isRightGrabbing;
-    Collider2D[] leftColliders;
-    Collider2D[] rightColliders;
     ContactFilter2D filter;
-    Vector3 leftGrabPosition;
-    Vector3 rightGrabPosition;
+    Vector2 leftGrabPosition;
+    Vector2 rightGrabPosition;
 
     void Start()
     {
@@ -29,66 +29,34 @@ public class Player : MonoBehaviour
         playerRB = transform.GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
         righHandRB.AddForceAtPosition(new Vector2(Input.GetAxis("Joystick X"), Input.GetAxis("Joystick Y")), rightHand.position, ForceMode2D.Impulse);
         leftHandRB.AddForceAtPosition(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")), leftHand.position, ForceMode2D.Impulse);
-        
-        if (Input.GetAxis("LeftTrigger") >= 0.5f)
+
+        Grabbing("LeftTrigger", ref leftHand, ref leftColliders, ref leftGrabPosition, ref leftHandRB, ref isLeftGrabbing);
+        Grabbing("RightTrigger", ref  rightHand, ref rightColliders, ref rightGrabPosition, ref righHandRB, ref isRightGrabbing);
+    }
+
+    private void Grabbing(string triggerInput, ref Transform hand, ref Collider2D[] colliders, ref Vector2 grabPosition, ref Rigidbody2D handRB, ref bool grabbingCheck)
+    {
+        if (Input.GetAxis(triggerInput) >= 0.5f)
         {
-            if (!isLeftGrabbing) {
-                leftColliders = new Collider2D[1];
-                leftHand.GetComponent<Collider2D>().OverlapCollider(filter, leftColliders);
-                leftGrabPosition = leftColliders[0].transform.InverseTransformPoint(leftHand.position); // we move that into the local space of the target, so we have a local position 
-            }
-
-            Debug.DrawLine(leftColliders[0].transform.TransformPoint(leftGrabPosition), leftHand.position, Color.red, 0.1f); // we then transform that out of the local space of target and back into world
-
-            Vector3 force = (leftColliders[0].transform.TransformPoint(leftGrabPosition) - leftHand.position) * playerRB.mass;
-
-            leftHandRB.velocity = Vector3.zero;
-
-            leftHandRB.AddForce(force, ForceMode2D.Impulse);
-        }
-
-        if (Input.GetAxis("LeftTrigger") < 0.5f)
-        {
-            if (isLeftGrabbing)
+            if (!grabbingCheck)
             {
-                leftColliders = null;
-                leftGrabPosition = Vector2.zero;
-            }
-        }
-
-        isLeftGrabbing = Input.GetAxis("LeftTrigger") > 0.5f ? true : false;
-
-        if (Input.GetAxis("RightTrigger") >= 0.5f)
-        {
-            if (!isRightGrabbing)
-            {
-                rightColliders = new Collider2D[1];
-                rightHand.GetComponent<Collider2D>().OverlapCollider(filter, rightColliders);
-                rightGrabPosition = rightColliders[0].transform.InverseTransformPoint(righHandRB.position); // we move that into the local space of the target, so we have a local position 
+                colliders = new Collider2D[1];
+                hand.GetComponent<Collider2D>().OverlapCollider(filter, colliders);
+                if (colliders[0] == null) return;
+                grabPosition = colliders[0].transform.InverseTransformPoint(hand.position); // we move that into the local space of the target, so we have a local position 
             }
 
-            Debug.DrawLine(rightColliders[0].transform.TransformPoint(rightGrabPosition), rightHand.position, Color.red, 0.1f); // we then transform that out of the local space of target and back into world
+            Vector2 force = (colliders[0].transform.TransformPoint(grabPosition) - hand.position) * playerRB.mass; // we then transform that out of the local space of target and back into world
 
-            Vector3 force = (rightColliders[0].transform.TransformPoint(rightGrabPosition) - rightHand.position) * playerRB.mass;
+            handRB.velocity = Vector2.zero;
 
-            righHandRB.velocity = Vector3.zero;
-
-            righHandRB.AddForce(force, ForceMode2D.Impulse);
+            handRB.AddForce(force, ForceMode2D.Impulse);
         }
 
-        if (Input.GetAxis("RightTrigger") < 0.5f)
-        {
-            if (isRightGrabbing)
-            {
-                rightColliders = null;
-                rightGrabPosition = Vector2.zero;
-            }
-        }
-
-        isRightGrabbing = Input.GetAxis("RightTrigger") > 0.5f ? true : false;
+        grabbingCheck = Input.GetAxis(triggerInput) > 0.5f ? true : false;
     }
 }
