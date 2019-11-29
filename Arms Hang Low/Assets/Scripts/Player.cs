436df8e032d26c1b131e618651fda3b5031d5e50
Hootlook <leetboom@live.fr>
2019-11-29 13:02:09 +0100

    Grabbing enhanced (now joints based)

--- Arms Hang Low/Assets/Scripts/Player.cs
+++ Arms Hang Low/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -38,25 +39,66 @@ public class Player : MonoBehaviour
         Grabbing("RightTrigger", ref  rightHand, ref rightColliders, ref rightGrabPosition, ref righHandRB, ref isRightGrabbing);
     }
 
-    private void Grabbing(string triggerInput, ref Transform hand, ref Collider2D[] colliders, ref Vector2 grabPosition, ref Rigidbody2D handRB, ref bool grabbingCheck)
+    private void Grabbing(string triggerInput, ref Transform hand, ref Collider2D[] colliders, ref Vector2 grabPosition, ref Rigidbody2D handRB, ref bool isGrabbing)
     {
         if (Input.GetAxis(triggerInput) >= 0.5f)
         {
-            if (!grabbingCheck)
+            if (!isGrabbing)
             {
                 colliders = new Collider2D[1];
                 hand.GetComponent<Collider2D>().OverlapCollider(filter, colliders);
+
                 if (colliders[0] == null) return;
-                grabPosition = colliders[0].transform.InverseTransformPoint(hand.position); // we move that into the local space of the target, so we have a local position 
+
+                if (colliders[0].GetComponent<Rigidbody2D>() == null) colliders[0].gameObject.AddComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+
+                if (colliders[0].GetComponent<Rigidbody2D>().bodyType == RigidbodyType2D.Static)
+                {
+                    colliders[0].gameObject.AddComponent<FixedJoint2D>().connectedBody = hand.GetComponent<Rigidbody2D>();
+                }
+                else
+                {
+                    colliders[0].gameObject.AddComponent<FixedJoint2D>().connectedBody = handRB;
+                }
             }
-
-            Vector2 force = (colliders[0].transform.TransformPoint(grabPosition) - hand.position) * playerRB.mass; // we then transform that out of the local space of target and back into world
-
-            handRB.velocity = Vector2.zero;
-
-            handRB.AddForce(force, ForceMode2D.Impulse);
+        }
+        else
+        {
+            if (isGrabbing)
+            {
+                var joints = colliders[0].GetComponents<Joint2D>();
+                foreach (var item in joints)
+                {
+                    if (item.connectedBody == hand.GetComponent<Rigidbody2D>() || item.connectedBody == handRB)
+                    {
+                        Destroy(item);
+                    }
+                }
+            }
         }
 
-        grabbingCheck = Input.GetAxis(triggerInput) > 0.5f ? true : false;
+        isGrabbing = Input.GetAxis(triggerInput) > 0.5f ? true : false;
     }
+
+    //private void Grabbing(string triggerInput, ref Transform hand, ref Collider2D[] colliders, ref Vector2 grabPosition, ref Rigidbody2D handRB, ref bool grabbingCheck)
+    //{
+    //    if (Input.GetAxis(triggerInput) >= 0.5f)
+    //    {
+    //        if (!grabbingCheck)
+    //        {
+    //            colliders = new Collider2D[1];
+    //            hand.GetComponent<Collider2D>().OverlapCollider(filter, colliders);
+    //            if (colliders[0] == null) return;
+    //            grabPosition = colliders[0].transform.InverseTransformPoint(hand.position); // we move that into the local space of the target, so we have a local position 
+    //        }
+
+    //        Vector2 force = (colliders[0].transform.TransformPoint(grabPosition) - hand.position) * playerRB.mass; // we then transform that out of the local space of target and back into world
+
+    //        handRB.velocity = Vector2.zero;
+
+    //        handRB.AddForce(force * force.magnitude, ForceMode2D.Impulse);
+    //    }
+
+    //    grabbingCheck = Input.GetAxis(triggerInput) > 0.5f ? true : false;
+    //}
 }
